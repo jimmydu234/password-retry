a1c5574b9ff39e613c383d20b9476bbfd18f75fa
Jimmy Du <Jimmy24753@gmail.com>
2020-12-21 20:34:24 +0800

    change

--- password-retry.py
+++ password-retry.py
@@ -1,13 +1,17 @@
 password = 'a123456'
 number = 3
 while number > 0:
+	number = number - 1
 	answer = input('請輸入密碼:')
 	if answer == password:
 		print('登出成功!')
 		break
 	else:
-		number = number - 1
-		print('密碼錯誤! 還有', number, '次機會')
+		print('密碼錯誤!')
+		if number > 0:
+			print('還有', number, '次機會')
+		else:
+			print('沒機會嘗試了! 要鎖帳號了啦!')	
 		
 
 	
